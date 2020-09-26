82dbe49544cd6e87b06bad9f7dd769a4c921d49b
Ameet Deshpande <ameetsd97@gmail.com>
2020-09-26 00:38:22 -0400

    Create smooth_loss.py

---
+++ smooth_loss.py
@@ -0,0 +1,15 @@
+"""
+Exponential moving window smoothing of loss function
+"""
+
+from typing import List
+
+def smooth(scalars: List[float], weight: float) -> List[float]:  # Weight between 0 and 1
+    last = scalars[0]  # First value in the plot (first timestep)
+    smoothed = list()
+    for point in scalars:
+        smoothed_val = last * weight + (1 - weight) * point  # Calculate smoothed value
+        smoothed.append(smoothed_val)                        # Save it
+        last = smoothed_val                                  # Anchor the last smoothed value
+
+    return smoothed
