00eb3da6e77eee90715313e7e3ba5b1f35b00b79
Ameet Deshpande <ameetsd97@gmail.com>
2021-08-11 23:59:11 -0400

    Update conda.sh

--- conda.sh
+++ conda.sh
@@ -1,2 +1,8 @@
 # Create a new environment
 conda create -n myenv python=3.8
+
+# Create environment from yml file
+conda env create -f environment.yml
+
+# Store environment
+conda env export > environment.yml
