f9f682c395b170a1c6ea0ef669b454bdb57d0dba
Ameet Deshpande <ameetsd97@gmail.com>
2021-10-04 13:24:17 -0400

    Update tmux.sh

--- bash/tmux.sh
+++ bash/tmux.sh
@@ -11,7 +11,9 @@ C-b c
 C-b %  C-b "
 
 # Navigating
-C-b <arrow key>
+C-b n (for next window) and C-b p (for the previous window)
+C-b #no (if window number is less than 10)
+C-b ' (followed by index number, for window numbers greater than or equal to 10)
 
 # Kill a session (here, the 0th session)
 tmux kill-session -t 0
