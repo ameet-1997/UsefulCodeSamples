224b03d3d691a78dafd37ff9ccf95f0540f8805d
Ameet Deshpande <ameetsd97@gmail.com>
2022-08-03 15:49:31 -0400

    Update add_args_to_config.py

--- transformers/add_args_to_config.py
+++ transformers/add_args_to_config.py
@@ -3,12 +3,13 @@ def add_args_to_config(config, model_args, data_args):
     """
     Add important arguments to the config
     """
+    prefix = 'csts'
     for arg in vars(model_args):
-        if 'csts' in arg:
-            setattr(config, arg, getattr(memory_bank_args, arg))
+        if prefix in arg:
+            setattr(config, arg, getattr(model_args, arg))
 
     for arg in vars(data_args):
-        if 'csts' in arg:
+        if prefix in arg:
             setattr(config, arg, getattr(data_args, arg))
 
     return config
