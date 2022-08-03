057d0f3e12e270846bdc994bc191ec4434d09474
Ameet Deshpande <ameetsd97@gmail.com>
2022-08-03 15:46:25 -0400

    Update add_args_to_config.py

--- transformers/add_args_to_config.py
+++ transformers/add_args_to_config.py
@@ -1,3 +1,4 @@
+# Example in https://github.com/ameet-1997/REMIND/blob/master/transformers/examples/pytorch/REMIND/utils_memory_bank.py
 def add_args_to_config(config, model_args, data_args):
     """
     Add important arguments to the config
@@ -9,3 +10,5 @@ def add_args_to_config(config, model_args, data_args):
     for arg in vars(data_args):
         if 'csts' in arg:
             setattr(config, arg, getattr(data_args, arg))
+
+    return config
